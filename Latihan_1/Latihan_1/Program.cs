152fdeb009d37f0e278967bc8b1990323993069d
fernanda258715-netizen <fernanda258715@student.smkn1kandeman.sch.id>
2025-11-03 21:42:42 +0700

    Add files via upload

---
+++ Latihan_1/Latihan_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Latihan_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Program menampilkan tabel perkalian 5
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"5 x {i} = {5 * i}");
+            }
+        }
+    }
+}
---
+++ Latihan_2/Latihan_2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Latihan_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1; // mulai dari 1
+            int total = 0; // untuk menyimpan hasil jumlah
+
+            while (i <=5) // selama i kurang dari atau sama dengan 5 
+            {
+                total += i; // tambahkan i ke total
+                Console.WriteLine($"Total = (total)");
+            }
+
+        }
+    }
+}
